f4234fc5d74201416ba2a3c5f129ee06fcdcd66d
Scott <weston.scot@gmail.com>
2013-08-23 23:16:59 +1200

    Work on the lights

--- src/Light.java
+++ src/Light.java
@@ -1,7 +1,16 @@
-import java.awt.Graphics;
+import java.awt.*;
+
+import javax.imageio.ImageIO;
+import java.io.*;
 
 
 public class Light implements Drawable{
+	private Image image;
+	
+	
+	public Light(String image){
+		this.image = ImageIO.read(new File(image));
+	}
 
     public void draw(Graphics g){
 
