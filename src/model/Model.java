ccdb2883907a7642e2dd69061ed85a84a29c930c
Tony <Tony@Florence>
2013-08-23 22:16:20 +1200

    put some stuff in model.

---
+++ src/model/Model.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.List;
+
+public class Model {
+	
+	List<Mob> mobs;
+	List<Static> statics;
+	Player player;
+	
+	public Model () {
+		
+		
+	}
+	
+	public void draw () {
+				
+	}
+	
+	// update the data 
+	public void tick () {
+		for ()		
+	}
+}
