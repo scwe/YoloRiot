bc093d2e77bf1bf91b7204af7e64682920286979
Scott <weston.scot@gmail.com>
2013-08-23 23:04:50 +1200

    Got the gui working

--- src/ScreenPanel.java
+++ src/ScreenPanel.java
@@ -1,15 +1,44 @@
 
-import java.awt.Graphics;
+import java.awt.*;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class ScreenPanel extends JPanel{
 
+	private YoloKeyboard key;
+	private YoloMouse mouse;
+	private Graphics2D buffer;
+	
     public ScreenPanel(){
+        setFocusable(true);
+        requestFocusInWindow();
+        mouse = new YoloMouse();
+        key = new YoloKeyboard();
+        
+        addMouseListener(mouse);
+        addMouseMotionListener(mouse);
+        addKeyListener(key);
+        
         
     }
-
+    
+    @Override
     public void paintComponent(Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+		Image offscreen = createImage(getWidth(), getHeight());
+		buffer = (Graphics2D) offscreen.getGraphics();
+		buffer.setColor(Color.white);
+		buffer.fillRect(0, 0, getWidth(), getHeight());
 
+		draw(buffer);
+		
+		g2d.drawImage(offscreen, 0, 0, this);
     }
+    
+    public void draw(Graphics2D g){
+    	g.drawRect(0, 0, YoloRiot.SCREEN_WIDTH, YoloRiot.SCREEN_HEIGHT);
+    	//do the drawing here
+    }
+    
+    
 }
--- src/YoloRiot.java
+++ src/YoloRiot.java
@@ -1,7 +1,29 @@
-public class YoloRiot{
+import java.awt.*;
+import javax.swing.*;
+
+public class YoloRiot extends JFrame{
+	public static final int SCREEN_WIDTH = 1024;
+	public static final int SCREEN_HEIGHT = 720;
+
+    private ScreenPanel screen;
 
     public YoloRiot(){
+    	setTitle("Yolo Riot");
+    	setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        screen = new ScreenPanel();
+        add(screen);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setFocusable(true);
+        setVisible(true);
         
+        gameLoop();
+    }
+    
+    public void gameLoop(){
+    	while(true){
+    		screen.repaint();
+    	}
     }
 
     public static void main(String[] args){
