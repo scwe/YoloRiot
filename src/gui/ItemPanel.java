2773b33de2766ee633a0eecffb2d5c32455776f3
Scott <weston.scot@gmail.com>
2013-08-24 19:01:54 +1200

    Working buttons =)

--- src/gui/ItemPanel.java
+++ src/gui/ItemPanel.java
@@ -4,15 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.*;
 import java.awt.event.*;
+import java.util.*;
 
 import image.ImageLoader;
 
 public class ItemPanel extends JPanel{
 	
+	private static final int BUTTON_WIDTH = 64;
+	private static final int BUTTON_HEIGHT = 64;
+	
+	private HashMap<String, String> buttonMap;
 	
 	public ItemPanel(){
+		buttonMap = new HashMap<String, String>();
+		buttonMap.put("Button.png", "test");
 		GridBagConstraints gc = new GridBagConstraints();
 		setPreferredSize(new Dimension(120, YoloRiot.SCREEN_HEIGHT));
+		add(newButton("Button.png"));
 	}
 	
 	public void paintComponent(Graphics g){
@@ -20,10 +28,11 @@ public class ItemPanel extends JPanel{
 	}
 	
 	public JButton newButton(String imageFile){
-		BufferedImage image = new ImageLoader().getImage(imageFile);
+		Image image = new ImageLoader().getImage(imageFile).getScaledInstance(BUTTON_WIDTH, BUTTON_HEIGHT, Image.SCALE_FAST);
 		
 		JButton button = new JButton(new ImageIcon(image));
-		button.setPreferredSize(new Dimension(64, 64));
+		button.setName(buttonMap.get(imageFile));
+		button.setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
 		
 		button.addActionListener(new ActionListener(){
 
