3328260cb1720658ac537423032ce4448efb8866
Scott <weston.scot@gmail.com>
2013-08-24 17:35:56 +1200

    Fixed spritesheet to use the ImageLoader

--- src/image/ImageLoader.java
+++ src/image/ImageLoader.java
@@ -1,18 +1,17 @@
 package image;
 
-import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class ImageLoader {
 	
-	public Image getImage(String name){
+	public BufferedImage getImage(String name){
 		
-		Image img;
+		BufferedImage img;
 			try{ img = ImageIO.read(ImageLoader.class.getResource(name)); }
 			catch (IOException e){ throw new Error(e); }
-		
 		
 		return img;
 	}
