89f4051e963165d96d0dbb61c7e28ce37e0e84d8
aterwl <aterwl@gmail.com>
2013-08-25 13:15:23 +1200

    fixed depcndancy issue

--- src/structureAIs/SimpleCannonAI.java
+++ src/structureAIs/SimpleCannonAI.java
@@ -3,7 +3,6 @@ package structureAIs;
 import model.Location;
 import model.Model;
 import projectiles.Projectile;
-import projectiles.SimpleProjectile;
 import structures.Structure;
 
 public class SimpleCannonAI implements StructureAI{
